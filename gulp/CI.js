88c4c5c51f509032c28f25b0a5f53f687bd1c622
zhengxi01 <zhengxi01@weidian.com>
2020-05-19 20:26:04 +0800

    预览

--- gulp/CI.js
+++ gulp/CI.js
@@ -21,7 +21,7 @@ const config = require('./config');
     loading.succeed('拷贝完成');
     loading.start('正在构建小程序npm包...');
     const project = new ci.Project({
-      appid: 'wx4b74228baa15489a',
+      appid: 'wx14656bb4e9966e9f',
       type: 'miniProgram',
       projectPath: config.build,
       privateKeyPath: path.resolve(__dirname, './private.wx14656bb4e9966e9f.key'),
@@ -35,19 +35,31 @@ const config = require('./config');
       }
     })
     loading.succeed('构建完成');
+   
+    loading.start('正在生成预览码...');
     // 完成构建npm之后，可用ci.preview或者ci.upload
-    // await ci.preview({
+    await ci.preview({
+      project,
+      desc: '预览测试',
+      setting: {
+        es6: true,
+      },
+      qrcodeFormat: 'image',
+      qrcodeOutputDest: `${config.build}/qrcode.png`, //二维码文件保存路径
+      onProgressUpdate: () => {},
+      // pagePath: `${config.build}/preview`, // 预览页面
+      // searchQuery: 'a=1&b=2',  // 预览参数 [注意!]这里的`&`字符在命令行中应写成转义字符`\&`
+      // proxy: 'your proxy url',
+    })
+    loading.succeed('预览码生成成功');
+    // await ci.upload({
     //   project,
-    //   desc: '预览测试',
+    //   version: '1.1.1',
+    //   desc: 'hello',
     //   setting: {
     //     es6: true,
     //   },
-    //   qrcodeFormat: 'image',
-    //   qrcodeOutputDest: `${config.build}/qrcode`, //二维码文件保存路径
     //   onProgressUpdate: console.log,
-    //   // pagePath: `${config.build}/preview`, // 预览页面
-    //   // searchQuery: 'a=1&b=2',  // 预览参数 [注意!]这里的`&`字符在命令行中应写成转义字符`\&`
-    //   // proxy: 'your proxy url',
     // })
   } catch (error) {
     loading.fail('构建失败');

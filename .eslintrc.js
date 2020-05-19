255c04ee541e2c6d6cc2eb2aa1ced8483f917bd3
zhengxi01 <zhengxi01@weidian.com>
2020-05-19 10:17:29 +0800

    a

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
-{
+module.exports = {
     "env": {
         "browser": true,
         "es6": true
     },
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended"
+        // "plugin:@typescript-eslint/eslint-recommended"
     ],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
-     "parser": "@typescript-eslint/parser",
+    //  "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": 11,
         "sourceType": "module"
     },
-    "plugins": [
-        "@typescript-eslint"
-    ],
+    // "plugins": [
+    //     "@typescript-eslint"
+    // ],
     "rules": {
         //代码检查
         //警告
